f9c8c4d9d1c9a35eb07d7fe885d5bf698cef9b14
thevictor <victor.d.alexa@gmail.com>
2024-10-01 18:36:15 +0300

    not found and error middleware

--- server.js
+++ server.js
@@ -20,6 +20,15 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
+
 // set spin-up function
 const PORT = process.env.PORT || 8080;
 
