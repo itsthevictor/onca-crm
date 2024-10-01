5d54ec89e017bab81ca94a6adc809a5e8a3f546e
thevictor <victor.d.alexa@gmail.com>
2024-10-01 18:00:04 +0300

    define dummy methods for user auth controller and routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,11 @@
-export const testController = async (req, res) => {
-  res.json("test controller");
+export const register = async (req, res) => {
+  res.json({ msg: "register user" });
+};
+
+export const login = async (req, res) => {
+  res.json({ msg: "login user" });
+};
+
+export const logout = async (req, res) => {
+  res.json({ msg: "logout user" });
 };
