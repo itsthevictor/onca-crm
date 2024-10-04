dc13aeef6029110df8a131f0df31b7ec3eba96d3
thevictor <victor.d.alexa@gmail.com>
2024-10-04 18:53:26 +0300

    layout components

--- client/src/layouts/Dashboard.jsx
+++ client/src/layouts/Dashboard.jsx
@@ -1,6 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Menu from "../components/Menu";
 const Dashboard = () => {
   return (
     <main>
+      <Navbar />
+      <Menu />
       <div className="dashboard-container">
         <Outlet />
       </div>
