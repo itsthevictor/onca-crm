77cc21a28b32653fe0afd9ba71d27ea7076b7f65
thevictor <victor.d.alexa@gmail.com>
2024-10-08 17:30:30 +0300

    jwt cookie at login

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import User from "../models/User.js";
 import StatusCodes from "http-status-codes";
 import { hashPassword, comparePassword } from "../utils/passwordUtils.js";
 import { UnauthenticatedError } from "../errors/customErrors.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
 export const register = async (req, res) => {
   const hashedPassword = await hashPassword(req.body.password);
@@ -14,10 +15,15 @@ export const login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
   const isValidUser =
     user && (await comparePassword(req.body.password, user.password));
-
   if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
-
-  res.status(StatusCodes.OK).json([user]);
+  const token = createJWT({ userId: user._id, role: user.role });
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.status(StatusCodes.OK).json({ msg: "user logged in" });
 };
 
 export const logout = async (req, res) => {
