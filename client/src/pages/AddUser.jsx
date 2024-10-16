7dc3fbcc04aefc5e03719cd8df11d5ad5e77ab8f
thevictor <victor.d.alexa@gmail.com>
2024-10-16 11:46:42 +0300

    tweak add user

--- client/src/pages/AddUser.jsx
+++ client/src/pages/AddUser.jsx
@@ -111,7 +111,7 @@ const AddUser = () => {
                   defaultChecked
                 />
                 <label htmlFor="temp" className="radio-label">
-                  account
+                  temp.
                 </label>
               </div>
             </div>
