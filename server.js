1294cdde1928ff1831fc8f1d39b81c803d69c16b
thevictor <victor.d.alexa@gmail.com>
2024-10-16 07:38:47 +0300

    setup client build in server

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ import * as dotenv from "dotenv";
 import morgan from "morgan";
 import { connectDB } from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
+import { dirname } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // init dotenv & setup app
 dotenv.config();
 const app = express();
+
+// path setup and clend build in public
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 // import middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -28,6 +35,10 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/partners", authenticateUser, partnerRouter);
 app.use("/api/v1/users", userRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+});
 
 app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
