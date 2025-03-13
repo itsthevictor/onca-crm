f5433098e6df3831ffc84629988bd42fea2cfd05
Victor Alexa <victor.d.alexa@gmail.com>
2025-03-13 21:39:32 +0200

    package updates

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // import modules
-import "express-async-errors";
-import express from "express";
-import * as dotenv from "dotenv";
-import morgan from "morgan";
-import { connectDB } from "./db/connectDB.js";
-import cookieParser from "cookie-parser";
-import { dirname } from "path";
-import path from "path";
-import { fileURLToPath } from "url";
+import 'express-async-errors';
+import express from 'express';
+import * as dotenv from 'dotenv';
+import morgan from 'morgan';
+import { connectDB } from './db/connectDB.js';
+import cookieParser from 'cookie-parser';
+import { dirname } from 'path';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // init dotenv & setup app
 dotenv.config();
@@ -15,35 +15,35 @@ const app = express();
 
 // path setup and client build in public
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./client/dist")));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 
 // import middleware
-import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
-import { authenticateUser } from "./middleware/authMiddleware.js";
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
 
 // import routes
-import authRouter from "./routes/authRouter.js";
-import partnerRouter from "./routes/partnerRouter.js";
-import userRouter from "./routes/userRouter.js";
-import companyRouter from "./routes/companyRouter.js";
+import authRouter from './routes/authRouter.js';
+import partnerRouter from './routes/partnerRouter.js';
+import userRouter from './routes/userRouter.js';
+import companyRouter from './routes/companyRouter.js';
 
 // middleware
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 app.use(cookieParser());
 app.use(express.json());
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/partners", authenticateUser, partnerRouter);
-app.use("/api/v1/users", userRouter);
-app.use("/api/v1/companies", authenticateUser, companyRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/partners', authenticateUser, partnerRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/companies', authenticateUser, companyRouter);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
-app.use("*", (req, res) => {
-  res.status(404).json({ msg: "not found" });
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' });
 });
 
 app.use(errorHandlerMiddleware);
